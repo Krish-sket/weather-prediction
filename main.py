8b40ecc31c3b7b3b6a9eb91838af8ce7e137f97c
Krish-sket <theworldofgamingksmvd@gmail.com>
2024-11-05 20:43:46 +0530

    weather prediction

---
+++ main.py
@@ -0,0 +1,39 @@
+
+from flask import Flask, render_template, request
+
+import json
+
+import urllib.request
+
+app=Flask(__name__)
+
+@app.route('/getWeather', methods=['POST', 'GET'])
+def weather():
+
+    if request.method == 'POST':
+        location = request.form['city']
+    else:
+        location='chennai'
+    api = 'e0b19566bba38f97d53743b5e55c8662'
+
+    try:
+        source = urllib.request.urlopen(
+        'http://api.openweathermap.org/data/2.5/weather?q=' + location +
+        '&appid=' + api).read()
+        responseData=json.loads(source)
+
+        data = {
+            "country_code":str(responseData['sys']['country']),
+            "temp":str(responseData['main']['temp']+ 'k'),
+            "location":str(responseData['name']),
+        }
+
+        return render_template('index.html', data=data)
+    except (Exception):
+        return render_template('index.html', error="Give the correct location")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(host='0.0.0.0',port=8080) 
